0d8923cac2580fe61207d6d580e30e462eb0c290
Vladimir Serbinenko <phcoder@gmail.com>
2020-03-06 03:39:24 +0100

    emscripten fixes

--- libretro-defs.h
+++ libretro-defs.h
@@ -4,4 +4,5 @@ void libretro_vfs_close(struct libretro_handle *h);
 int libretro_vfs_getc(struct libretro_handle *h);
 void libretro_vfs_putc(int c, struct libretro_handle *h);
 void libretro_vfs_flush(struct libretro_handle *h);
-void scr_mark_dirty ();
+void scr_mark_dirty (void);
+void tty_set_keymap (void);
